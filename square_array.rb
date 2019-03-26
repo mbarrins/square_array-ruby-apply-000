556feac9039a620775a79a03ee32bd5b5a63176c
Maija Barrins <maija.barrins@gmail.com>
2019-03-26 23:02:43 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,5 @@
 def square_array(array)
-  #squares = []
-  #array.each{|x| squares << x**2}
-  #squares
-
-  array.collect{|x| x**2}
+  squares = []
+  array.each{|x| squares << x**2}
+  squares
 end
